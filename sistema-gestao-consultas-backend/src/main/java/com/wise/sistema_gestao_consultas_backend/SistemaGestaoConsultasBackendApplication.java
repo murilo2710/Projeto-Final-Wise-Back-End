3dec91c90f66e875a402da549c7f3baa33d131ef
Murilo Regnier Stange <murilostange27@gmail.com>
2026-05-11 22:51:21 -0300

    chore(backend): inicializa estrutura Spring Boot e configuracao MySQL

---
+++ sistema-gestao-consultas-backend/src/main/java/com/wise/sistema_gestao_consultas_backend/SistemaGestaoConsultasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wise.sistema_gestao_consultas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoConsultasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoConsultasBackendApplication.class, args);
+	}
+
+}
---
+++ sistema-gestao-consultas-backend/src/test/java/com/wise/sistema_gestao_consultas_backend/SistemaGestaoConsultasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wise.sistema_gestao_consultas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoConsultasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
